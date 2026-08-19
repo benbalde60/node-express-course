196ec23d7ba4f26d3b2ef933749c1548aae80a4f
Ben Balde <benbalde60@gmail.com>
2026-08-18 23:56:25 -0500

    Update express tutorial app and dependencies

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,28 @@
-console.log('Express Tutorial')
+const http = require('http');
+const {readFileSync}= require('fs');
+//get all files
+const homepage = readFileSync('./index.html');
+const server = http.createServer((req,res)=>{
+    const url = req.url;
+//home page
+    if(url === '/'){
+    console.log(req.url)
+    res.writeHead(200,{'content-type':'text/html'})
+    res.write(homepage);
+    res.end();
+//about page
+} else if(url === '/about') {
+    res.writeHead(200,{'content-type':'text/html'})
+    res.write('<h1>About Page </h1>');
+    res.end();
+//404
+} else {
+    res.writeHead(404,{'content-type':'text/html'})
+    res.write('<h1>404 page Not Found</h1>');
+    res.end();
+}
+})
+    
+    
+
+server.listen(5000)
